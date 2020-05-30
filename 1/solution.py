11b40127f0155614c5edee4e154827b2e41159fc
james <jameshaller27@gmail.com>
2020-05-29 23:09:47 -0700

    Update and clarify project 1 solution

--- 1/solution.py
+++ 1/solution.py
@@ -1,15 +1,27 @@
-def run_once() -> bool:
+def run_once():
+    '''
+    Get input from the user and perform math.
+    Returns True if the program should end. Otherwise, returns False.
+    '''
+
+    # Get the first number, which may be "exit"
     o1 = input('#1> ')
 
+    # If the user entered "exit", return True, meaning stop
     if o1 == 'exit':
-        return False
+        return True
 
+    # If we're still here, that means the first number isn't "exit"
+
+    # Get the second number and the operation
     o2 = input('#2> ')
     op = input('op> ')
 
+    # Convert the operands to integers
     o1 = int(o1)
     o2 = int(o2)
 
+    # Decide which operation to perform and print the result
     if op == '+':
         print(o1 + o2)
     elif op == '-':
@@ -19,12 +31,9 @@ def run_once() -> bool:
     elif op == '/':
         print(o1 / o2)
 
-    return True
+    return False
 
-def main():
-    lcv = run_once()
-    while lcv:
-        lcv = run_once()
 
-main()
-
+done = run_once()
+while not done:
+    done = run_once()
